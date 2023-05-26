dee9f5cdd011ef433f353b668c8f56046a961aaa
MichelBDLC <michelbdlc@gmail.com>
2023-05-26 16:52:03 -0700

    db errors fixed

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -3,7 +3,7 @@ const pgp = require('pg-promise')();
 require('dotenv').config();
 DATABASE_URL = process.env.DATABASE_URL;
 
-const cn = DATABSE_URL ? {
+const cn = DATABASE_URL ? {
     connectionString: DATABASE_URL,
     max:30,
 } : {
